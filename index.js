3908633f5d592d0184fbe4a34e3bb0e8051f37ea
Arthur <arthur@corenzan.com>
2019-03-15 16:12:08 -0300

    Fix channel health monitoring

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
+#!/usr/bin/env node
+
 const WebSocket = require("ws");
 
 const channels = {};
 
-function getChannel(path) {
-  if (path in channels) {
-    return channels[path];
-  }
-  const channel = [];
-  channels[path] = channel;
-  console.log(new Date(), "channel created", path);
-  const timer = setInterval(() => {
-    let shouldDeleteChannel = true;
-    channel.forEach(client => {
-      if (client.keepAlive === false) {
-        client.terminate();
-      } else {
-        client.keepAlive = shouldDeleteChannel = false;
-        client.ping(function() {});
-      }
-    });
-    if (shouldDeleteChannel) {
-      console.log(new Date(), "channel deleted", path);
-      delete channels[path];
-      clearInterval(timer);
+function noop() {}
+
+function log(action, ...params) {
+  console.log(new Date(), action, ...params);
+}
+
+function healthMonitor(channel) {
+  for (let i = 0; i < channel.clients.length; i++) {
+    const client = channel.clients[i];
+
+    if (client.keepAlive === false) {
+      client.terminate();
+      channel.clients.splice(i, 1);
+    } else {
+      client.keepAlive = false;
+      client.ping(noop);
     }
-  }, 10 * 1000);
+  }
+
+  if (channel.clients.length === 0) {
+    clearInterval(channel.timer);
+    delete channels[channel.key];
+    log("channel deleted", channel.key);
+  }
+}
+
+function getChannel(key) {
+  if (key in channels) {
+    return channels[key];
+  }
+
+  const channel = {
+    key,
+    timer: setInterval(() => healthMonitor(channel), 30 * 1000),
+    clients: []
+  };
+  channels[key] = channel;
+
+  log("channel created", key);
+
   return channel;
 }
 
-function getRemoteAddress(request) {
-  return;
+function broadcast(channel, sender) {
+  for (let i = 0; i < channel.clients.length; i++) {
+    const client = channel.clients[i];
+    if (client !== sender && client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  }
 }
 
 const server = new WebSocket.Server({
@@ -39,46 +63,37 @@ const server = new WebSocket.Server({
 server.on("connection", (client, request) => {
   const url = new URL("http://localhost" + request.url);
   const channel = getChannel(url.pathname);
-  channel.push(client);
+  channel.clients.push(client);
 
   client.remoteAddress =
     "x-forwarded-for" in request.headers
       ? request.headers["x-forwarded-for"].split(/\s*,\s*/)[0]
       : request.connection.remoteAddress;
 
-  const remoteAddr = console.log(
-    new Date(),
-    "client connected",
-    client.remoteAddress,
-    url.pathname
-  );
+  log("client connected", url.pathname, client.remoteAddress);
 
   client.on("pong", () => {
     client.keepAlive = true;
   });
 
-  client.on("close", code => {
-    console.log(
-      new Date(),
+  client.on("close", (code, reason) => {
+    log(
       "client disconnected",
+      url.pathname,
       client.remoteAddress,
-      url.pathname
+      code,
+      reason
     );
   });
 
   client.on("message", message => {
-    console.log(
-      new Date(),
-      "message",
-      client.remoteAddress,
+    log(
+      "message received",
       url.pathname,
-      message.length
+      client.remoteAddress,
+      `${message.length} bytes`
     );
 
-    channel.forEach(c => {
-      if (c !== client && c.readyState === WebSocket.OPEN) {
-        c.send(message);
-      }
-    });
+    broadcast(channel, client);
   });
 });
